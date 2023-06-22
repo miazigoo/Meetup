9317d7577f36c6c5f1d19dc3c738418b6df7ec30
Bloodov Pavel <kasper6024@gmail.com>
2023-06-23 02:02:11 +0300

    Добавлено: Модели и админка

--- bot_meetup/admin.py
+++ bot_meetup/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(Topic)
+admin.site.register(Speaker)
+admin.site.register(Client)
+admin.site.register(Event)
+admin.site.register(Flag)
--- bot_meetup/models.py
+++ bot_meetup/models.py
@@ -1,3 +1,91 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    telegram_id = models.IntegerField(unique=True)
+    nik_name = models.CharField(
+        max_length=256,
+        null=True,
+        blank=True,
+        verbose_name="UserName"
+    )
+
+    def __str__(self):
+        return f"{self.nik_name}"
+
+    class Meta:
+        verbose_name = "Клиент"
+        verbose_name_plural = "Клиенты"
+
+
+class Speaker(models.Model):
+    client = models.ForeignKey(Client,
+                               on_delete=models.CASCADE,
+                               related_name='speakers')
+    name = models.CharField(
+        max_length=256,
+        null=True,
+        blank=False,
+        verbose_name="Name"
+    )
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name = "Спикер"
+        verbose_name_plural = "Спикеры"
+
+
+class Topic(models.Model):
+    speaker = models.ManyToManyField(Speaker)
+    topic = models.CharField(
+        max_length=256,
+        null=True,
+        blank=False,
+        verbose_name="Name"
+    )
+
+    def __str__(self):
+        return f"{self.topic}"
+
+    class Meta:
+        verbose_name = "Тема"
+        verbose_name_plural = "Темы"
+
+
+class Event(models.Model):
+    title = models.CharField(
+        max_length=256,
+        null=True,
+        blank=False,
+        verbose_name="Title"
+    )
+    text = models.TextField()
+    speaker = models.ManyToManyField(Speaker)
+    topic = models.ManyToManyField(Topic)
+
+    def __str__(self):
+        return f"{self.title}"
+
+    class Meta:
+        verbose_name = "Мероприятие"
+        verbose_name_plural = "Мероприятия"
+
+
+class Flag(models.Model):
+    speaker = models.ForeignKey(Speaker,
+                                on_delete=models.CASCADE,
+                                related_name='speakers_flag')
+    flag = models.BooleanField(
+        default=False,
+        null=True,
+        blank=False,
+    )
+
+    def __str__(self):
+        return f"{self.speaker} | {self.flag}"
+
+    class Meta:
+        verbose_name = "Флаг"
+        verbose_name_plural = "Флаги"
